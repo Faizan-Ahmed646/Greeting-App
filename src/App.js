72841c16a37e3c1f1db02ecbda5076571253660d
No Future <faizanahmed3742@gmail.com>
2022-02-08 06:18:57 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+const cssStyle={ };
 
 function App() {
+  let Greeting="";
+  let curDate= new Date();
+  curDate=curDate.getHours();
+  if(curDate>=6 && curDate<11){
+    Greeting= "Good Morning"
+    cssStyle.color="Green";
+  }
+  else if(curDate>=11 && curDate<17){
+    Greeting= "Good Afternoon"
+    cssStyle.color="#D1D100";
+  }
+  else if(curDate>=17 && curDate<21){
+    Greeting= "Good Evening"
+    cssStyle.color="Orange";
+  }
+  else {
+    Greeting= "Good Night"
+    cssStyle.color="Black";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+      <h1>Hello Sir, <span style={cssStyle}>{Greeting}</span></h1>
+      </div>
+      {/* <div className="hello">
+        <h5>
+        Created by <span>Faizan Ahmed</span>
+        </h5>
+      </div> */}
+      </>
   );
 }
 
